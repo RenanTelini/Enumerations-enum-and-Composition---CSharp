2547247f34850fa762490eb70c0732fa9e63d31d
Renan Telini <renan_telini@hotmail.com>
2026-08-26 21:42:41 -0300

    Work with Enum

---
+++ README.md
@@ -0,0 +1,7 @@
+# Enumerations (enum) and Composition - C#
+
+#### This exercise is based on the <a href="https://www.udemy.com/course/programacao-orientada-a-objetos-csharp/?couponCode=MT260714G2">"C# COMPLETO Programação Orientada a Objetos + Projetos"</a> course.
+
+### <ins>Work with Enum</ins>
+
+#### What is an enumeration or enum? A: It is a special type used to literally specify a set of related constants.
---
+++ WorkWithEnum/WorkWithEnum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WorkWithEnum.Entities;
+using WorkWithEnum.Entities.Enums;
+
+namespace WorkWithEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
